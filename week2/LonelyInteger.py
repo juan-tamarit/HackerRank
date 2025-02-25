d9ec7cdb47230c487553f5b76933377671e5966a
juan-tamarit <juan.tamarit@outlook.es>
2025-02-25 23:14:17 +0100

    Task 8

---
+++ week2/LonelyInteger.py
@@ -0,0 +1,66 @@
+"""Given an array of integers, where all elements but one occur twice, find the unique element.
+
+Example
+
+The unique element is .
+
+Function Description
+
+Complete the lonelyinteger function in the editor below.
+
+lonelyinteger has the following parameter(s):
+
+int a[n]: an array of integers
+Returns
+
+int: the element that occurs only once
+Input Format
+
+The first line contains a single integer, , the number of integers in the array.
+The second line contains  space-separated integers that describe the values in .
+
+Constraints
+
+It is guaranteed that  is an odd number and that there is one unique element.
+"""
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'lonelyinteger' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts INTEGER_ARRAY a as parameter.
+#
+
+def lonelyinteger(a):
+    sol={}
+    for n in a:
+        if n in sol:
+            sol[n]=False
+        else:
+            sol[n]=True
+    n=0
+    for clave,booleano in sol.items():
+        if booleano:
+            n=clave
+            break
+    return n
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    a = list(map(int, input().rstrip().split()))
+
+    result = lonelyinteger(a)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
