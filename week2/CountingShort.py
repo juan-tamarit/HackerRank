fd2990399a8be62d2a2319f27aca7aa55813e8ea
juan-tamarit <juan.tamarit@outlook.es>
2025-03-11 15:25:08 +0100

    Task 12

---
+++ week2/CountingShort.py
@@ -0,0 +1,27 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'countingSort' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts INTEGER_ARRAY arr as parameter.
+#
+
+def countingSort(arr):
+    sol=[0]*100
+    for n in arr:
+        sol[n]=sol[n]+1
+    return sol
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    arr = list(map(int, input().rstrip().split()))
